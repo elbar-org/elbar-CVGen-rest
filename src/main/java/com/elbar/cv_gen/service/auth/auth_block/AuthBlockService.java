c1cf9847d33d23d2b913ff908f012e1b4ce5f5d0
Nurislom373 <nurislomx72@gmail.com>
2022-11-02 16:58:39 +0500

    add AuthBlockService APIs #15

--- src/main/java/com/elbar/cv_gen/service/auth/auth_block/AuthBlockService.java
+++ src/main/java/com/elbar/cv_gen/service/auth/auth_block/AuthBlockService.java
@@ -4,9 +4,10 @@ import com.elbar.cv_gen.criteria.auth.auth_block.AuthBlockCriteria;
 import com.elbar.cv_gen.dto.auth.auth_block.AuthBlockCreateDTO;
 import com.elbar.cv_gen.dto.auth.auth_block.AuthBlockDetailDTO;
 import com.elbar.cv_gen.dto.auth.auth_block.AuthBlockGetDTO;
+import com.elbar.cv_gen.service.BaseService;
 import com.elbar.cv_gen.service.GenericGLService;
 
-public interface AuthBlockService extends GenericGLService<AuthBlockGetDTO, AuthBlockCriteria, Integer> {
+public interface AuthBlockService extends GenericGLService<AuthBlockGetDTO, AuthBlockCriteria, Integer>, BaseService {
 
     void create(AuthBlockCreateDTO dto);
 
